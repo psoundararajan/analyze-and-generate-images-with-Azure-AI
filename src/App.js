76071a5c3d63b7a39c6a86489e7a9bb41ca7c43c
Prasad Soundararajan <p5su@pge.com>
2024-11-12 23:43:36 -0800

    added a static page

--- src/App.js
+++ src/App.js
@@ -1,8 +1,44 @@
-import React from 'react';
+import React, {useState} from 'react';
+
 
 function App() {
-  const value = 'World';
-  return <div>Hello {value}</div>;
+  const [text, setText] = useState('');
+
+  const handleButtonClick = (buttonText) => {
+    console.log(buttonText + ' clicked');
+  }
+  var styles = {
+    inputsContainer: {
+      flex: 1
+    },
+    fullWidthButton: {
+      backgroundColor: 'blue',
+      height:70,
+      flexDirection: 'row',
+      justifyContent: 'center',
+      alignItems: 'center'
+    },
+    fullWidthButtonText: {
+      fontSize:24,
+      color: 'white'
+    },
+    input: {
+      padding: "20 px",
+      width: "400px",
+      height: 20,
+    },
+  }
+  return <div>
+    <h1>Computer vision</h1>
+    <div>Insert URL or type prompt:</div>
+    <div>
+   <input type="text" value={text} placeholder = "Enter URL to analyze or textual prompt to generate an image" style={styles.input} onChange={(e) => setText(e.target.value)} />
+   </div>
+    <div>
+   <button onClick={() => handleButtonClick('Analyze')}>Analyze</button>
+   <button onClick={() => handleButtonClick('Generate')}>Generate</button>
+   </div>
+  </div>;
 }
 
 export default App;
